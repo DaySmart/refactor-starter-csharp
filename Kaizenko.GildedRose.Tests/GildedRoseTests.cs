e05d7a1f0dda6128679087c942fa59e2fd823796
Trevor Olson <teolson17@gmail.com>
2022-08-31 11:28:41 -0700

    GildedRoses -- added functionality for Conjured items

--- Kaizenko.GildedRose.Tests/GildedRoseTests.cs
+++ Kaizenko.GildedRose.Tests/GildedRoseTests.cs
@@ -1,14 +1,88 @@
 ﻿using NUnit.Framework;
 using System;
+using System.Collections.Generic;
 
 namespace Kaizenko.GildedRose.Tests
 {
     public class GildedRoseTests
     {
         [Test]
-        public void MyFirstTest()
+        public void UpdateQualityWhenConjuredItemQuality25_ExpectsQuailityTo23()
         {
-            Assert.Fail("Not yet implemented");
+            var gildedRose = new GildedRose(new List<Item>() {
+                new Item("Conjured", 50, 25)
+            });
+
+            gildedRose.UpdateQuality();
+
+            Assert.That(gildedRose.Items[0].Quality, Is.EqualTo(23));
+        }
+
+        [Test]
+        public void UpdateQualtiyWhenConjuredItemQuality1_ExpectsQuality0()
+        {
+
+            var gildedRose = new GildedRose(new List<Item>() {
+                new Item("Conjured", 50, 1)
+            });
+
+            gildedRose.UpdateQuality();
+
+            Assert.That(gildedRose.Items[0].Quality, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void UpdateQualityWhenSellInIsZeroAndQuality25_ExpectsQuality21()
+        {
+
+            var gildedRose = new GildedRose(new List<Item>() {
+                new Item("Conjured", 0, 25)
+            });
+
+            gildedRose.UpdateQuality();
+
+            Assert.That(gildedRose.Items[0].Quality, Is.EqualTo(21));
+        }
+
+
+        [Test]
+        public void UpdateQualityWhenNormalItemSellin50Quality25_ExpectsQuality24()
+        {
+
+            var gildedRose = new GildedRose(new List<Item>() {
+                new Item("Normal Item", 50, 25)
+            });
+
+            gildedRose.UpdateQuality();
+
+            Assert.That(gildedRose.Items[0].Quality, Is.EqualTo(24));
+        }
+
+        [Test]
+        public void UpdateQualityWhenNormalItemSellin0Quality25_ExpectsQuality23()
+        {
+
+            var gildedRose = new GildedRose(new List<Item>() {
+                new Item("Normal Item", 0, 25)
+            });
+
+            gildedRose.UpdateQuality();
+
+            Assert.That(gildedRose.Items[0].Quality, Is.EqualTo(23));
+        }
+
+
+        [Test]
+        public void UpdateQualityWhenNormalItemSellin50Quality0_ExpectsQuality0()
+        {
+
+            var gildedRose = new GildedRose(new List<Item>() {
+                new Item("Normal Item", 50, 0)
+            });
+
+            gildedRose.UpdateQuality();
+
+            Assert.That(gildedRose.Items[0].Quality, Is.EqualTo(0));
         }
     }
 
--- Kaizenko.GildedRose/GildedRose.cs
+++ Kaizenko.GildedRose/GildedRose.cs
@@ -5,7 +5,7 @@ namespace Kaizenko.GildedRose
 {
     public class GildedRose
     {
-        IList<Item> Items;
+        public IList<Item> Items;
         public GildedRose(IList<Item> Items)
         {
             this.Items = Items;
@@ -15,14 +15,14 @@ namespace Kaizenko.GildedRose
         {
             for (var i = 0; i < Items.Count; i++)
             {
+                // normal item, not special conditions
                 if (Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
                 {
                     if (Items[i].Quality > 0)
                     {
-                        if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                        {
-                            Items[i].Quality = Items[i].Quality - 1;
-                        }
+                        // when we are are a normal item, with larger than 0 quantity, reduce our quality
+                        // by 1
+                        this.UpdateItemQuality(Items[i]);
                     }
                 }
                 else
@@ -59,16 +59,16 @@ namespace Kaizenko.GildedRose
 
                 if (Items[i].SellIn < 0)
                 {
+                    // we past the the sell by date now
                     if (Items[i].Name != "Aged Brie")
                     {
+                        // We are either normal item, conjured, legendary item, or a backstage pass
                         if (Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
                         {
+                            // we either normal item, curjured, or legendary
                             if (Items[i].Quality > 0)
                             {
-                                if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                                {
-                                    Items[i].Quality = Items[i].Quality - 1;
-                                }
+                                this.UpdateItemQuality(Items[i]);
                             }
                         }
                         else
@@ -84,6 +84,25 @@ namespace Kaizenko.GildedRose
                         }
                     }
                 }
+
+                // for all items we should never go below 0 in quality
+                if (Items[i].Quality < 0 )
+                {
+                    Items[i].Quality = 0;
+                }
+            }
+        }
+
+
+        protected void UpdateItemQuality(Item item)
+        {
+            if (item.Name == "Conjured")
+            {
+                item.Quality = item.Quality - 2;
+            }
+            else if (item.Name != "Sulfuras, Hand of Ragnaros")
+            {
+                item.Quality = item.Quality - 1;
             }
         }
     }
