5e34564b93b25d24aca2f2a4b5f124b1bce048cf
Mike Berryman <michael.berryman@daysmart.com>
2022-09-01 10:34:52 -0400

    setup Approvals library

--- Kaizenko.ExpenseReport.Tests/ExpenseReportTests.cs
+++ Kaizenko.ExpenseReport.Tests/ExpenseReportTests.cs
@@ -1,28 +1,22 @@
-﻿using NUnit.Framework;
+﻿using ApprovalTests.Reporters;
+using NUnit.Framework;
 using System;
 using System.IO;
-using Kaizenko.ExpenseReport;
 
 namespace Kaizenko.ExpenseReport.Tests
 {
     public class ExpenseReportTests
     {
         [Test]
-        public void WhenNoExpensesProvided_ExpectAnEmptyReport()
+        [UseReporter(typeof(DiffReporter))]
+        public void PrintReport_WhenNoExpensesProvided_ExpectAnEmptyReport()
         {
             using (StringWriter sw = new StringWriter())
             {
                 Console.SetOut(sw);
-
                 ExpenseReport expenseReport = new ExpenseReport();
                 expenseReport.PrintReport(new System.Collections.Generic.List<Expense>());
-                string consoleOutput = sw.ToString();
-                string[] lines = consoleOutput.Split('\n');
-
-                Assert.That(lines.Length, Is.EqualTo(4));
-                Assert.That(lines[0], Contains.Substring("Expenses"));
-                Assert.That(lines[1], Is.EqualTo("Meal expenses: 0"));
-                Assert.That(lines[2], Is.EqualTo("Total expenses: 0"));
+                ApprovalTests.Approvals.Verify(sw);
             }
         }
     }
