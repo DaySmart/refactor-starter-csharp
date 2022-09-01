28806572c95877f9c451a6661bc96b5679343169
Mike Berryman <michael.berryman@daysmart.com>
2022-09-01 12:00:02 -0400

    refactoring into methods

--- Kaizenko.ExpenseReport/ExpenseReport.cs
+++ Kaizenko.ExpenseReport/ExpenseReport.cs
@@ -26,30 +26,9 @@ namespace Kaizenko.ExpenseReport
 
             foreach (Expense expense in expenses)
             {
-                if (expense.type == ExpenseType.DINNER || expense.type == ExpenseType.BREAKFAST)
-                {
-                    mealExpenses += expense.amount;
-                }
-
-                String expenseName = "";
-                switch (expense.type)
-                {
-                    case ExpenseType.DINNER:
-                        expenseName = "Dinner";
-                        break;
-                    case ExpenseType.BREAKFAST:
-                        expenseName = "Breakfast";
-                        break;
-                    case ExpenseType.CAR_RENTAL:
-                        expenseName = "Car Rental";
-                        break;
-                }
-
-                String mealOverExpensesMarker =
-                    expense.type == ExpenseType.DINNER && expense.amount > 5000 ||
-                    expense.type == ExpenseType.BREAKFAST && expense.amount > 1000
-                        ? "X"
-                        : " ";
+                mealExpenses = CalculateMealExpenses(mealExpenses, expense);
+                string expenseName = GetExpenseName(expense);
+                string mealOverExpensesMarker = GetMealOverExpenseMarker(expense);
 
                 Console.WriteLine(expenseName + "\t" + expense.amount + "\t" + mealOverExpensesMarker);
 
@@ -58,6 +37,42 @@ namespace Kaizenko.ExpenseReport
 
             Console.WriteLine("Meal expenses: " + mealExpenses);
             Console.WriteLine("Total expenses: " + total);
+        }
+
+        private static string GetMealOverExpenseMarker(Expense expense)
+        {
+            return expense.type == ExpenseType.DINNER && expense.amount > 5000 || expense.type == ExpenseType.BREAKFAST && expense.amount > 1000
+                                    ? "X"
+                                    : " ";
+        }
+
+        private static string GetExpenseName(Expense expense)
+        {
+            string expenseName = "";
+            switch (expense.type)
+            {
+                case ExpenseType.DINNER:
+                    expenseName = "Dinner";
+                    break;
+                case ExpenseType.BREAKFAST:
+                    expenseName = "Breakfast";
+                    break;
+                case ExpenseType.CAR_RENTAL:
+                    expenseName = "Car Rental";
+                    break;
+            }
+
+            return expenseName;
+        }
+
+        private static int CalculateMealExpenses(int mealExpenses, Expense expense)
+        {
+            if (expense.type == ExpenseType.DINNER || expense.type == ExpenseType.BREAKFAST)
+            {
+                mealExpenses += expense.amount;
+            }
+
+            return mealExpenses;
         }
 
         protected virtual DateTime GetDate()
