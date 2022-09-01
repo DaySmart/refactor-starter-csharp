07e9fcfbad0b722de36dd795f03befc70fa6d6f3
Murat Kara <murat.kara@daysmart.com>
2022-09-01 11:52:02 -0400

    Added expense get list

--- Kaizenko.ExpenseReport.Tests/ExpenseReportTests.cs
+++ Kaizenko.ExpenseReport.Tests/ExpenseReportTests.cs
@@ -2,6 +2,7 @@
 using NUnit.Framework;
 using System;
 using System.IO;
+using System.Collections.Generic;
 
 namespace Kaizenko.ExpenseReport.Tests
 {
@@ -19,7 +20,36 @@ namespace Kaizenko.ExpenseReport.Tests
                 ApprovalTests.Approvals.Verify(sw);
             }
         }
+
+        [Test]
+        [UseReporter(typeof(DiffReporter))]
+        public void PrintReport_Legacy()
+        {
+            using (StringWriter sw = new StringWriter())
+            {
+                
+                Console.SetOut(sw);
+                ExpenseReport expenseReport = new MyExpenseReport();
+                expenseReport.PrintReport(getList());
+                ApprovalTests.Approvals.Verify(sw);
+            }
+        }
+
+        private List<Expense> getList()
+        {
+            List<Expense> expenses = new List<Expense>();
+            ExpenseType[] expenseTypes = { ExpenseType.DINNER, ExpenseType.BREAKFAST, ExpenseType.CAR_RENTAL };
+            foreach (ExpenseType expenseType in expenseTypes)
+                for (int amount = 500; amount <= 8000; amount += 500)
+                    expenses.Add(new Expense()
+                    {
+                        type = expenseType,
+                        amount = amount
+                    });
+            return expenses;
+        }
     }
+
 
     public class MyExpenseReport : ExpenseReport
     {
