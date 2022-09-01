208f304b944da1f08efdcd3d3456f03aa6e03c2b
Shereef Marzouk <shereef.marzouk@daysmart.com>
2022-09-01 08:11:26 -0600

    Added basic test `WhenNoExpensesProvided_ExpectAnEmptyReport`

--- Kaizenko.ExpenseReport.Tests/ExpenseReportTests.cs
+++ Kaizenko.ExpenseReport.Tests/ExpenseReportTests.cs
@@ -1,14 +1,29 @@
 ﻿using NUnit.Framework;
 using System;
+using System.IO;
+using Kaizenko.ExpenseReport;
 
 namespace Kaizenko.ExpenseReport.Tests
 {
     public class ExpenseReportTests
     {
         [Test]
-        public void MyFirstTest()
+        public void WhenNoExpensesProvided_ExpectAnEmptyReport()
         {
-            Assert.Fail("Not yet implemented");
+            using (StringWriter sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+
+                ExpenseReport expenseReport = new ExpenseReport();
+                expenseReport.PrintReport(new System.Collections.Generic.List<Expense>());
+                string consoleOutput = sw.ToString();
+                string[] lines = consoleOutput.Split('\n');
+
+                Assert.That(lines.Length, Is.EqualTo(4));
+                Assert.That(lines[0], Contains.Substring("Expenses"));
+                Assert.That(lines[1], Is.EqualTo("Meal expenses: 0"));
+                Assert.That(lines[2], Is.EqualTo("Total expenses: 0"));
+            }
         }
     }
 }
